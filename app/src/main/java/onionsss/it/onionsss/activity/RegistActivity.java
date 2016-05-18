a18bb38150987d2203f5e312fbea68f5961a0df4
zhangqi <759308541@qq.com>
2016-05-18 08:57:23 +0800

    注册小bug

--- app/src/main/java/onionsss/it/onionsss/activity/RegistActivity.java
+++ app/src/main/java/onionsss/it/onionsss/activity/RegistActivity.java
@@ -1,15 +1,11 @@
 package onionsss.it.onionsss.activity;
 
 import android.app.ProgressDialog;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,8 +17,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import okhttp3.Response;
 import onionsss.it.onionsss.R;
-import onionsss.it.onionsss.bean.User;
-import onionsss.it.onionsss.dao.UserDao;
 import onionsss.it.onionsss.utils.OkUtils;
 
 public class RegistActivity extends AppCompatActivity {
@@ -40,7 +34,7 @@ public class RegistActivity extends AppCompatActivity {
     Button regist_btn_regist;
 
     // private UserDao ud;
-    private static final String REGISTPATH = "http://192.168.1.108:8080/onionsss/RegistServlet";
+    private static final String REGISTPATH = "http://169.254.163.120:8080/onionsss/RegistServlet";
     private ProgressDialog mProgressDialog;
 
 
@@ -177,7 +171,7 @@ public class RegistActivity extends AppCompatActivity {
                     Response response = OkUtils.postResponse(REGISTPATH, json);
                     if (response.code() == 200) {
                         //TODO  将服务端response返回值改为json类型
-                        switch (Integer.parseInt(response.toString())) {
+                        switch (Integer.parseInt(response.body().string())) {
                             //服务端解析错误
                             case -1:
                                 handler.sendEmptyMessage(REGIST_JSON);
